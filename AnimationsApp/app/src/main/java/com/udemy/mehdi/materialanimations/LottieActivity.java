8a67dd1c94395dcd8391faa2a7db191a81d81320
John Doe <powergame_coder2@yahoo.com>
2018-03-22 11:01:15 +0430

    Lottie worked

--- AnimationsApp/app/src/main/java/com/udemy/mehdi/materialanimations/LottieActivity.java
+++ AnimationsApp/app/src/main/java/com/udemy/mehdi/materialanimations/LottieActivity.java
@@ -2,12 +2,30 @@ package com.udemy.mehdi.materialanimations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+//Animation files courtesy of https://www.lottiefiles.com/aep
+
 
 public class LottieActivity extends AppCompatActivity {
+
+    LottieAnimationView heart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lottie);
+        heart = findViewById(R.id.heartAnimation);
+        heart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                heart.invalidate();
+                if(heart.isAnimating()) heart.pauseAnimation();
+                else heart.playAnimation();
+            }
+        });
     }
 }
