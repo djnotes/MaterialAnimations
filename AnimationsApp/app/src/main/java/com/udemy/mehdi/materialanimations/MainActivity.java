cfd3d33a60ed0aed4d27a8f171d37a2d592a8d81
John Doe <powergame_coder2@yahoo.com>
2018-03-25 01:32:37 +0430

    Added circular reveal

--- AnimationsApp/app/src/main/java/com/udemy/mehdi/materialanimations/MainActivity.java
+++ AnimationsApp/app/src/main/java/com/udemy/mehdi/materialanimations/MainActivity.java
@@ -1,13 +1,19 @@
 package com.udemy.mehdi.materialanimations;
 
+import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.transition.TransitionManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements MyAdapter.OnItemClickListener{
@@ -43,18 +49,40 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.OnItemC
 
     @Override
     public void onItemClick(View view, int position) {
+
         int titleRes = MyAdapter.TITLES[position];
         switch(titleRes) {
             case R.string.animation_move_view:
                 performAnimation();
                 break;
-
             case R.string.animation_lottie:
                 startActivity(new Intent(mContext, LottieActivity.class));
                 break;
             case R.string.constraint_layout_animations:
                 startActivity(new Intent(mContext, ConstraintLayoutActivity.class));
                 break;
+            case R.string.ripple:
+                startActivity(new Intent(mContext, RippleActivity.class));
+                break;
+            case R.string.circular_reveal:
+                initCircularReveal(view);
+                break;
         }
+    }
+
+    private void initCircularReveal(View view) {
+        boolean isVeggie = ((ColorDrawable)view.getBackground()) != null && ((ColorDrawable)view.getBackground()).getColor() == Color.GREEN;
+
+        TransitionManager.beginDelayedTransition((ViewGroup) view);
+        double finalRadius = Math.hypot(view.getWidth()/2, view.getHeight() / 2);
+
+        if (isVeggie) {
+            view.setBackgroundColor(Color.WHITE);
+        } else {
+            Animator anim = ViewAnimationUtils.createCircularReveal(view, (int) view.getWidth()/2, (int) view.getHeight()/2, 0, (float)finalRadius);
+            view.setBackgroundColor(Color.GREEN);
+            anim.start();
+        }
+
     }
 }
